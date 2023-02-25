1a71e5843e6f9a5a138c2f02d33dbab320980e25
Sacripant <yas.zirari@gmail.com>
2023-02-25 18:44:59 +0100

    styles upagrade

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const button = document.querySelector(".press-button");
-const onclick = document.querySelector(".press-button");
 const btn1 = document.getElementById('btn-1');
 const btn2 = document.getElementById('btn-2');
 const answer = document.querySelector('p');
-
-
-
 
 btn1.addEventListener('click', () => {
     answer.classList.add('showAnswer')
@@ -24,20 +19,22 @@ btn2.addEventListener('click', () => {
 const souris = document.querySelector(".mousemove");
 const questionContainer = document.querySelector(".box")
 
+
+
 window.addEventListener("mousemove", (e) => {
     souris.style.left = e.pageX + "px";
     souris.style.top = e.pageY + "px"; 
 });
 
-window.addEventListener("mousedown", () => {
-    souris.style.transform = "scale(2) translate(-25%, -25%)";
-    souris.style.transition = "0.3s ease"
- })
-
 window.addEventListener("mouseup", () => {
     souris.style.transform = "scale(1) translate(-50%, -50%)";
     souris.style.transition = "none"
 })
+
+window.addEventListener("mousedown", () => {
+    souris.style.transform = "scale(2) translate(-25%, -25%)";
+    souris.style.transition = "0.3s ease"
+ })
  
 questionContainer.addEventListener("mouseenter", () => {
     questionContainer.style.opacity = "50%"
@@ -46,3 +43,4 @@ questionContainer.addEventListener("mouseenter", () => {
 questionContainer.addEventListener("mouseout", () => {
     questionContainer.style.opacity = "100%"
 })
+
