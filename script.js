0236b2cc6f495bf2c90503d823240b0ff7feccb8
Sacripant <yas.zirari@gmail.com>
2023-02-26 01:36:50 +0100

    sound add

--- script.js
+++ script.js
@@ -83,7 +83,6 @@ nextBtn.addEventListener('click', () => {
     else if(step == 3) {
         navigator.vibrate(200) // vibrate 200ms
         alert("Gagné tu merit de m'epouser :)");
-        beat.play();
     }
 })
 
